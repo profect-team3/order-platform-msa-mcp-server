2e1f395e2f3dbffcaac842235b369504bdd4f370
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2025-08-21 15:22:53 +0900

    feat(mcp) : appconfig 정리

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,28 +1,15 @@
 package app.config;
 
-import java.time.Duration;
-
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-import app.commonSecurity.TokenPrincipalParser;
-import lombok.RequiredArgsConstructor;
 
 @Configuration
-@RequiredArgsConstructor
 public class AppConfig {
-	private final TokenPrincipalParser tokenPrincipalParser;
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder
-			.additionalInterceptors((req, body, ex) -> {
-				tokenPrincipalParser.tryGetAccessToken()
-					.ifPresent(token -> req.getHeaders().setBearerAuth(token));
-				return ex.execute(req, body);
-			})
-			.connectTimeout(Duration.ofSeconds(5))
-			.build();
+		return builder.build();
 	}
 }
